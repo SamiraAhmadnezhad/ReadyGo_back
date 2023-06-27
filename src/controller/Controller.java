ff94672dfe46b3ff98fde7a5c4d1edf06af02a70
Samira Ahmadnezhad <samiraahmadnezhad2004@gmail.com>
2023-06-27 17:52:29 +0430

    check sing up

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,50 +3,65 @@ package controller;
 import database.Database;
 import utils.Convertor;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.*;
 import java.util.Scanner;
 
 public class Controller {
-//    private String get(HashMap<String ,String > data){
-//        int count =Integer.valueOf(data.get("count"));
-//        ArrayList<HashMap<String,String>> list=Database.getInstance().getTable("massages").get();
-//        ArrayList<HashMap<String,String>> finalList=new ArrayList<>();
-//        for (int i=list.size()-1;i>=Math.max(0,list.size()-count);i--){
-//            finalList.add(list.get(i));
-//        }
-//        return Convertor.arrMapToString(finalList);
-//    }
-    private String getBy(){
-        return "";
+    private String giveAllBooks() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("src/data/books.txt"));
+        String result="";
+        while(scan.hasNextLine()) {
+            result += scan.nextLine()+",,";
+        }
+        scan.close();
+        //System.out.println(result);
+        return result;
+    }
+    private String checkSingUp(String data) throws IOException {
+        String[] emailAndPassAndusername = data.split(",,");
+        Scanner scan = new Scanner(new File("src/data/users.txt"));
+        while(scan.hasNextLine()) {
+            String s = scan.nextLine();
+            if (s.contains(emailAndPassAndusername[2]+",,")) {
+                scan.close();
+                return "username is unavailable";
+            }
+        }
+        FileWriter fileWriter=new FileWriter("src/data/users.txt" ,true);
+        String s=emailAndPassAndusername[2]+",,"+emailAndPassAndusername[0]+",,"+emailAndPassAndusername[1]+",, ,, ,, ,, ,,0,,false,, ,,0,,0000/0/0";
+        System.out.println(s.split(",,").length);
+        fileWriter.write(s+"\n");
+        fileWriter.close();
+        scan.close();
+        return "Sing up successfully\n"+s;
     }
 
     private String checkLogin(String data) throws FileNotFoundException {
         String[] emailAndPass = data.split(",,");
-        System.out.println(emailAndPass[0]+emailAndPass[1]);
-        Scanner scan = new Scanner(new File("src/data/massages.txt"));
+        Scanner scan = new Scanner(new File("src/data/users.txt"));
         while(scan.hasNextLine()){
             String s=scan.nextLine();
             if (s.contains(emailAndPass[0]+",,"))
-                if (!s.contains(emailAndPass[1]+",,"))
+                if (!s.contains(emailAndPass[1]+",,")) {
+                    scan.close();
                     return "Password is incorrect!";
-                else
-                    return "Login successfully\n"+s;
+                }
+                else {
+                    scan.close();
+                    return "Login successfully\n" + s;
+                }
         }
+        scan.close();
         return "User not found!";
     }
-    public  String run (String command, String data) throws FileNotFoundException {
-        //HashMap<String , String> dataMap= Convertor.stringToMap(data);
+    public  String run (String command, String data) throws IOException {
         switch (command){
-//            case "get":
-//                return get(dataMap);
-            case "getBy":
-                return getBy();
+            case "checkSingUp":
+                return checkSingUp(data);
             case "checkLogin":
                 return checkLogin(data);
+            case "giveAllBooks":
+                return giveAllBooks();
         }
         return "eshteb zadi!!!";
     }
